1d3cec5c6a35ea075cac6f42e640b7decf70274e
payal hossain <payalhossain508@gmail.com>
2025-04-13 19:42:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Navber from './assets/component/navber/Navber'
+
+function App() {
+  
+
+  return (
+    <>
+  <h1 className='text-3xl text-cyan-400'>hasi to you lendf me</h1>
+  <Navber></Navber>
+    </>
+  )
+}
+
+export default App
